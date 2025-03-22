d14cc193777bfd06e0cd17c85bd9aad8d14d70b9
FreedAInew <166154591+FreedAInew@users.noreply.github.com>
2025-03-21 19:28:08 -0500

    Update CorsConfig.java

--- src/main/java/only/get/infrastructure/config/CorsConfig.java
+++ src/main/java/only/get/infrastructure/config/CorsConfig.java
@@ -21,8 +21,7 @@ public class CorsConfig {
             "http://localhost:3000",  // Permitir React o Frontend
             "http://localhost:5500",
             "http://127.0.0.1:5500", // Origen exacto de tu frontend
-            "http://127.0.0.1:5501",
-            "https://error-production-f565.up.railway.app" // ✅ Agregar Railway con HTTPS
+            "http://127.0.0.1:5501"
             ));   // Permitir otro origen como Live Server en VS Code)); // Orígenes permitidos
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
